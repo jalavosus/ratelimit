3bec1a471bd090134b207abfb5eafa32560eea95
James Alavosus <alavosus.james@gmail.com>
2021-10-12 03:26:52 -0400

    Move Wrap() and WrapContext() to wrap.go for readability

--- ratelimit.go
+++ ratelimit.go
@@ -33,28 +33,6 @@ type (
 	RateLimiterFn func() error
 )
 
-// Wrap wraps the passed function within another function, which no parameters
-// and calls RateLimit using f(). Helpful for building functions which are "preloaded" with
-// a rate limiter, rather than calling RateLimit around every function call.
-func Wrap(f func() error, callsPerInterval int64, interval time.Duration) WrappedFn {
-	r := NewRateLimiter(callsPerInterval, interval)
-
-	return func() error {
-		return r.RateLimit(f)
-	}
-}
-
-// WrapContext wraps the passed function within another function, which takes a single context.Context parameter
-// and calls RateLimitContext using f() and that context. Helpful for building functions which are "preloaded" with
-// a rate limiter, rather than calling RateLimitContext around every function call.
-func WrapContext(f func() error, callsPerInterval int64, interval time.Duration) WrappedFnContext {
-	r := NewRateLimiter(callsPerInterval, interval)
-
-	return func(ctx context.Context) error {
-		return r.RateLimitContext(ctx, f)
-	}
-}
-
 // RateLimiter is a fancy little struct which can do everything you want it to and more
 // in the realm of rate limiting.
 type RateLimiter struct {
